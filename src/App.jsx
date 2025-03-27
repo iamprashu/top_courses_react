bb1607898bf5fe20fc5323a04f79ad98653eb2b0
iamprashu <iamprashu.gs@gmail.com>
2025-03-28 01:19:48 +0530

    App Component and Css Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './TailwindCompiled.css'
 import Filter from './Components/Filter';
+import Heading from './Components/Heading';
+import Cards from './Components/Cards';
 function App(){
-    return(<div className='bg-red-800 h-screen w-screen'>
+    return(<div className='bg-[#000000] h-screen w-screen'>
+        <Heading></Heading>
         <Filter></Filter>
+        <Cards></Cards>
     </div>)
 }
 
