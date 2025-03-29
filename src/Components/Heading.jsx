1604cd2298b58f109b2e6276d66a1a8ecb8cc901
iamprashu <iamprashu.gs@gmail.com>
2025-03-29 13:52:12 +0530

    Updates Css and Data

--- src/Components/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import '../TailwindCompiled.css'
-
-function Heading(){
-  return(<div className='bg-[#1c1c1c] h-[40px] text-2xl text-center text-white font-serif font-bold'>
-    Top Course Selection
-  </div>)
-}
-
-export default Heading
