c297ac37268f0079ccc3fe848144ae893dc54de3
iamprashu <iamprashu.gs@gmail.com>
2025-03-30 00:57:37 +0530

    Updated Mobile and Desktop Navigation Components

--- src/Components/MobileMenu.jsx
+++ src/Components/MobileMenu.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 import '../TailwindCompiled.css'
-import { filterData } from '../includes/data'
-function MobileMenu(){
+
+function MobileMenu(props){
+
+  let filterData = props.filterData
+  let setCatogary = props.setCatogary;
+  let setIsMobileMenuOpen= props.setIsMobileMenuOpen;
+  let isBodyClicked = props.isBodyClicked;
+  let catogary = props.catogary;
+
+  function filterClickHandler(data){
+    setIsMobileMenuOpen(false);
+    setCatogary(data);
+  }
+
+
+
   return(
     <div className="w-[100%]">
-      <ul className={`flex flex-col justify-center w-[100%] gap-2`}>
+      <ul className={`flex flex-col justify-center w-[100%] gap-1 backdrop-blur-2xl bg-black/10`}>
         {filterData.map((filterObj)=>
-          <li key={filterObj.id} className='bg-gray-700 h-10 text-2xl flex items-center justify-around text-white  hover:font-extrabold '>{filterObj.title}</li>
+          <li key={filterObj.id} className={`backdrop-blur-2xl p-2 h-10 text-2xl flex items-center justify-around text-white  hover:font-extrabold ${catogary === filterObj.title ? 'border-2':''}`} onClick={()=>{filterClickHandler(filterObj.title)}}>{filterObj.title}</li>
         )}
       </ul>
     </div>
