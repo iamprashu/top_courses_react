4d26e4655fb4b90d5cb71d75c4fce6599c98c6b3
iamprashu <iamprashu.gs@gmail.com>
2025-03-29 13:52:34 +0530

    Update App Coponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import Navigation from './Components/Navigation';
 import './TailwindCompiled.css'
-import Filter from './Components/Filter';
-import Heading from './Components/Heading';
+import { apiUrl } from './includes/data';
 import Cards from './Components/Cards';
+import { useEffect,useState } from 'react';
+import { toast } from 'react-toastify';
+import Spinner from './Components/Spinner';
+
 function App(){
-    return(<div className='bg-[#000000] h-screen w-screen'>
-        <Heading></Heading>
-        <Filter></Filter>
-        <Cards></Cards>
-    </div>)
+  const[courses,setCourses] = useState(null)
+  const[loading,setLoading] = useState(true);
+
+      async function getData(){
+        setLoading(true);
+        try{
+
+          let rawResponse = await fetch(apiUrl);
+          let jsonResponse = await  rawResponse.json();
+          setCourses(jsonResponse);
+        }catch(error){
+          toast.error('error')
+        }
+        setLoading(false)
+      }
+
+      useEffect(()=>{getData()},[])
+  return(
+    <div className='bg-gray-900 h-screen w-[100%] flex flex-col'>
+      <Navigation></Navigation>
+      {loading ? (<Spinner/>) : (<Cards courses={courses}/>)}
+    </div>
+  )
 }
 
 export default App;
