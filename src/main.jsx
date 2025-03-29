5618fb533d5a2351a960bd0acc4ed9c9bf74e030
iamprashu <iamprashu.gs@gmail.com>
2025-03-29 13:52:50 +0530

    Update Main Component

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './TailwindCompiled.css'
 import App from './App.jsx'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/ReactToastify.css'
+// import 'react-toastify/dist/ReactTostify.css';
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <div>
+        <App/>
+        <ToastContainer></ToastContainer>
+    </div>
 )
